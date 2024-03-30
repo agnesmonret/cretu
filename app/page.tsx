d73a7afa8c3807c38c1e4bfcbee024ac368423e2
AGN <157490898+agnesmonret@users.noreply.github.com>
2024-03-30 14:59:32 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -99,52 +99,7 @@ function AboutMe() {
   );
 }
 
-function ContactLink({
-  href,
-  title,
-  website,
-  email,
-}: {
-  email?: string;
-  href?: string;
-  title: string;
-  website?: string;
-}) {
-  return (
-    <span className="block items-center gap-4">
-      {website && <p className="text-quaternary">{website}</p>}
-      {href && (
-        <a
-          className="text-secondary hover:text-primary transition-opacity duration-150"
-          href={href}
-          rel="noopener noreferrer"
-          target="_blank"
-        >
-          {title}{' '}
-          <svg
-            className=" inline-block h-3 w-3"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth={1.5}
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M4.5 19.5l15-15m0 0H8.25m11.25 0v11.25"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            />
-          </svg>
-        </a>
-      )}
-      {email && (
-        <p className="text-secondary hover:text-primary transition-opacity duration-150">
-          {title}
-        </p>
-      )}
-    </span>
-  );
-}
+
 
 async function RecentWritings() {
   const { posts } = (await getData()).props;
